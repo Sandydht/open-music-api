d45adb6c7029c331c0a5d01d7a375df19e07e4c4
Sandydht <sandydwi75@gmail.com>
2023-04-24 17:23:20 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require('module-alias/register');
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
+
