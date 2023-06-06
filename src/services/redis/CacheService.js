384ddf764d6d443871a35eb12865212b7455c1a6
Sandydht <sandydwi75@gmail.com>
2023-06-06 15:01:14 +0700

    fix: cache expired

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -17,7 +17,7 @@ class CacheService {
     this.client.connect();
   }
 
-  async set(key, value, expirationInSecond = 30000) {
+  async set(key, value, expirationInSecond = 1800) {
     await this.client.set(key, value, {
       EX: expirationInSecond,
     });
